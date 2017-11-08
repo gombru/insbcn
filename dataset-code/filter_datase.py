119d50ea2267278c49817680af4dd31669702eea
raulgomez <raulgombru@gmail.com>
2017-11-08 09:47:27 +0100

    Many things

--- dataset-code/filter_datase.py
+++ dataset-code/filter_datase.py
@@ -3,27 +3,26 @@ import json
 from shutil import copyfile
 from langdetect import detect, detect_langs
 
-
-filtered_jsons_dir = "../../datasets/instaBarcelona/jsons_filtered/"
-output_file_path = "../../datasets/instaBarcelona/captions.json"
+filtered_jsons_dir = "../../../hd/datasets/instaBarcelona/json_filtered/"
+output_file_path = "../../../hd/datasets/instaBarcelona/captions.json"
 
 word_TH = 4
 
-data = load("../../datasets/instaBarcelona/json_toy/")
+data = load("../../../hd/datasets/instaBarcelona/json/")
 print "Number of jsons: " + str(len(data))
 
-#Load users blacklist
+# Load users blacklist
 users_blacklist = []
-users_blacklist_file = open("../../datasets/instaBarcelona/users_blacklist.txt","r")
-for line in users_blacklist:
-    users_blacklist.append(line.replace('\n',''))
+users_blacklist_file = open("../../../hd/datasets/instaBarcelona/users_blacklist.txt", "r")
+for line in users_blacklist_file:
+    users_blacklist.append(line.replace('\n', ''))
 users_blacklist_file.close()
 
-#City names to filter: We'll filter images containing other city names appart from barcelona. That will discard spam
+# City names to filter: We'll filter images containing other city names appart from barcelona. That will discard spam
 cities = []
-cities_file = open("../../datasets/instaBarcelona/cities.txt","r")
+cities_file = open("../../../hd/datasets/instaBarcelona/cities.txt", "r")
 for line in cities_file:
-    cities.append(line.replace('\n',''))
+    cities.append(line.replace('\n', ''))
 cities_file.close()
 
 discarded_by_user = 0
@@ -32,22 +31,27 @@ discarded_by_short_caption = 0
 discarded_by_nul_caption = 0
 discarded_by_lan = 0
 
-
 output_data = {}
 
+c = 0
+
 for k, v in data.iteritems():
+
+    c += 1
+    if c % 50000 == 0:
+        print c
 
     # Discard by user
     if v['owner']['id'] in users_blacklist:
         discarded_by_user += 1
-        if discarded_by_user % 10 == 0:
+        if discarded_by_user % 500 == 0:
             print "Num of posts dicarded by user: " + str(discarded_by_user)
         continue
 
     # Check if post has caption
-    if 'caption'not in v:
+    if 'caption' not in v:
         discarded_by_nul_caption += 1
-        if discarded_by_nul_caption % 1 == 0:
+        if discarded_by_nul_caption % 500 == 0:
             print "Num of posts dicarded by no caption: " + str(discarded_by_nul_caption)
         continue
 
@@ -60,7 +64,7 @@ for k, v in data.iteritems():
     # Check num of words. Discard if under the threshold
     if len(words) < word_TH:
         discarded_by_short_caption += 1
-        if discarded_by_short_caption % 10 == 0:
+        if discarded_by_short_caption % 500 == 0:
             print "Num of posts dicarded by short caption: " + str(discarded_by_short_caption)
         continue
 
@@ -69,33 +73,39 @@ for k, v in data.iteritems():
     for city in cities:
         if city in caption:
             discarded_by_city += 1
-            if discarded_by_city % 10 == 0:
+            if discarded_by_city % 500 == 0:
                 print "Num of posts dicarded by city: " + str(discarded_by_city)
             cur_discarded = True
             break
     if cur_discarded: continue
 
     # Check language
-    languages = detect_langs(caption)
-    languages_names = []
-    for l in languages:
-        languages_names.append(l.lang)
+    try:
+        languages = detect_langs(caption)
+        languages_names = []
+        for l in languages:
+            languages_names.append(l.lang)
 
-    if 'en' not in languages_names and 'es' not in languages_names and 'ca' not in languages_names:
+        if 'en' not in languages_names and 'es' not in languages_names and 'ca' not in languages_names:
+            discarded_by_lan += 1
+            if discarded_by_lan % 500 == 0:
+                print "Num of posts dicarded by language: " + str(discarded_by_lan)
+            continue
+    except:
+        print "Error detecting language, continuing"
         discarded_by_lan += 1
-        if discarded_by_lan % 10 == 0:
-            print "Num of posts dicarded by language: " + str(discarded_by_lan)
         continue
-
 
     # Else save the data in a dir with keys the id and with values the captions (originals)
     output_data[k] = v['caption']
 
-    #And save the original json in a separate folder
+    # And save the original json in a separate folder
     with open(filtered_jsons_dir + k + '.json', 'w') as outfile:
         json.dump(v, outfile)
 
-print "Discards: No captions: " + str(discarded_by_nul_caption) + " Short: " + str(discarded_by_short_caption) + " City: " + str(discarded_by_city) + " User: " + str(discarded_by_user) + " Language: " + str(discarded_by_lan)
+print "Discards: No captions: " + str(discarded_by_nul_caption) + " Short: " + str(
+    discarded_by_short_caption) + " City: " + str(discarded_by_city) + " User: " + str(
+    discarded_by_user) + " Language: " + str(discarded_by_lan)
 print "Number of original vs resulting elements: " + str(len(output_data)) + " vs " + str(len(data))
 
 print "Saving JSON"
--- dataset-code/resize_dataset.py
+++ dataset-code/resize_dataset.py
@@ -12,30 +12,39 @@ im_dest_path = "../../../hd/datasets/instaBarcelona/img_resized/"
 
 minSize = 256
 
-def resize(file):
 
+def resize(file):
     try:
-        im = Image.open(file)\
+
+        im = Image.open(file)
 
         w = im.size[0]
         h = im.size[1]
 
+        # print "Original w " + str(w)
+        # print "Original h " + str(h)
+
         if w < h:
             new_width = minSize
-            new_height = minSize * (h/w)
+            new_height = int(minSize * (float(h) / w))
 
         if h <= w:
             new_height = minSize
-            new_width = minSize * (w/h)
+            new_width = int(minSize * (float(w) / h))
 
+        # print "New width "+str(new_width)
+        # print "New height "+str(new_height)
         im = im.resize((new_width, new_height), Image.ANTIALIAS)
         im.save(im_dest_path + file.split('/')[-1])
     except:
-        print "Failed copying image: "
-        return
+        print "Failed copying image. Removing image and caption"
+        os.remove(file)
+        os.remove(file.replace("img", "json").replace("jpg", "json"))
 
+        print "Removed"
+        return
 
 
 if not os.path.exists(im_dest_path):
     os.makedirs(im_dest_path)
-Parallel(n_jobs=8)(delayed(resize)(file) for file in glob.glob(images_path + "/*.jpg"))
+Parallel(n_jobs=12)(delayed(resize)(file) for file in glob.glob(images_path + "/*.jpg"))
