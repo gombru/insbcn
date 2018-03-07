08c0fb866c9f00b37dddb9c93df78c2c3527eb95
raulgomez <raulgombru@gmail.com>
2018-03-07 12:43:28 +0100

    Many things

--- datasetCode/get_users_blacklist.py
+++ datasetCode/get_users_blacklist.py
@@ -3,12 +3,11 @@ import plotly.plotly as py
 from load_jsons import *
 import collections
 
+posts_TH = 100  # Will discard users having more than this publications, they are probably spam
 createBlacklist = True
 
 data = load("../../../hd/datasets/instaBarcelona/json/")
 print "Number of jsons: " + str(len(data))
-
-posts_TH = 50  # Will discard users having more than 100 publications, they are probably spam
 
 # Plot num of publications of top  users
 users = {}
@@ -21,7 +20,7 @@ for k, v in data.iteritems():
 print "Number of users: " + str(len(users))
 print "User with max publications has:  " + str(max(users.values()))
 
-topX = 100
+topX = 5000
 user_publis_sorted = users.values()
 user_publis_sorted.sort(reverse=True)
 x = range(topX)
@@ -44,36 +43,36 @@ if createBlacklist:
 
 
 # Plot num of likes
-likes_tens = {}
-likes_hundreds = {}
-
-for k, v in data.iteritems():
-    likes_ten = round(v['likes']['count'] / 10)
-    if likes_ten not in likes_tens:
-        likes_tens[likes_ten] = 1
-    else:
-        likes_tens[likes_ten] = likes_tens[likes_ten] + 1
-
-
-print "Number of different likes values (ten): " + str(len(likes_tens))
-print "Publication with max likes has (ten):  " + str(max(likes_tens.values()))
-
-topX = 10
-
-likes_tens_ordered = collections.OrderedDict(sorted(likes_tens.items()))
-
-if len(likes_tens_ordered.values()) < topX:
-    print "Not enought likes to print tens"
-else:
-    likes_publis_sorted = likes_tens_ordered.values()
-    likes_publis_sorted.sort(reverse=True)
-    my_xticks = ['0-5', '5-15', '15-25', '25-35','35-45', '45-55', '55-65', '75-85', '85-95','95-105']
-    x = range(topX)
-    plt.xticks(x, my_xticks)
-    width = 1/1.5
-    plt.bar(x, likes_publis_sorted[0:topX], width, color="blue")
-    plt.title("Likes of top posts (tens)")
-    plt.show()
+# likes_tens = {}
+# likes_hundreds = {}
+#
+# for k, v in data.iteritems():
+#     likes_ten = round(v['likes']['count'] / 10)
+#     if likes_ten not in likes_tens:
+#         likes_tens[likes_ten] = 1
+#     else:
+#         likes_tens[likes_ten] = likes_tens[likes_ten] + 1
+#
+#
+# print "Number of different likes values (ten): " + str(len(likes_tens))
+# print "Publication with max likes has (ten):  " + str(max(likes_tens.values()))
+#
+# topX = 10
+#
+# likes_tens_ordered = collections.OrderedDict(sorted(likes_tens.items()))
+#
+# if len(likes_tens_ordered.values()) < topX:
+#     print "Not enought likes to print tens"
+# else:
+#     likes_publis_sorted = likes_tens_ordered.values()
+#     likes_publis_sorted.sort(reverse=True)
+#     my_xticks = ['0-5', '5-15', '15-25', '25-35','35-45', '45-55', '55-65', '75-85', '85-95','95-105']
+#     x = range(topX)
+#     plt.xticks(x, my_xticks)
+#     width = 1/1.5
+#     plt.bar(x, likes_publis_sorted[0:topX], width, color="blue")
+#     plt.title("Likes of top posts (tens)")
+#     plt.show()
 
 
 print "Done"
